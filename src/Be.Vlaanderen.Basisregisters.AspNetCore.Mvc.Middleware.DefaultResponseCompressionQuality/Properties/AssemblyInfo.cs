731f2771adbe6f658b6b30fcead9e5ad284014d6
David Cumps <david@cumps.be>
2018-12-17 10:51:31 +0100

    feat: open source with MIT license as 'agentschap Informatie Vlaanderen'

---
+++ src/Be.Vlaanderen.Basisregisters.AspNetCore.Mvc.Middleware.DefaultResponseCompressionQuality/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyProduct("Vlaamse basisregisters")]
+[assembly: AssemblyDescription("ASP.NET Core MVC Middleware to define default compression quality priorities.")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: AssemblyCopyright("Copyright (c) Vlaamse overheid")]
+[assembly: AssemblyCompany("Vlaamse overheid")]
+
+[assembly: Guid("dbf8d3ac-1973-4f66-b1d2-aeae5138faf9")]
+
+[assembly: NeutralResourcesLanguage("en")]
